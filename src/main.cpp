305585371e8171dc4131a345f0071c8d410faedd
MariaPastuhova <masapastuhova777@gmail.com>
2025-02-27 23:14:27 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 extern int Sum(int a, int b);
+extern int Mult(int a, int b);
 
 int main()
 {
@@ -8,6 +9,9 @@ int main()
 	int b = 8;
 
 	int c = Sum(5, 4);
+	int d = Mult(2, 7);
 
 	std::cout << c;
+
+	std::cout << "\n" << d;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -3,3 +3,7 @@
 int Sum(int a, int b) {
 	return a + b;
 }
+
+int Mult(int a, int b) {
+	return a * b;
+}
